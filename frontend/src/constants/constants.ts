3489f71e7b1b6e660c5658ec3c329a93e2ee9a23
hansel.tjahyadi <hansel.tjahyadi@shopee.com>
2023-10-04 17:09:54 +0700

    user can now login. At the moment only the token is received and no error handling

--- frontend/src/constants/constants.ts
+++ frontend/src/constants/constants.ts
@@ -1,3 +1,4 @@
 export const URL = "http://localhost:8080";
 export const HEADER = "application/json";
 export const REGISTER = "/register";
+export const LOGIN = "/login";
--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -1,17 +1,48 @@
-import React from "react";
+import { useState } from "react";
 import Input from "../../components/Input/Input";
 import Button from "../../components/Button/Button";
 import LoginRegisterNav from "../../components/LoginRegisterNav/LoginRegisterNav";
 import LoginRegisterSideBar from "../../components/LoginRegisterSideBar/LoginRegisterSideBar";
+import { URL, HEADER, LOGIN } from "../../constants/constants";
 
 function Login() {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const submitTest = async (e: any) => {
+    e.preventDefault();
+
+    const loginData = {
+      method: "POST",
+      headers: {
+        "Content-Type": HEADER,
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    };
+
+    try {
+      const response = await fetch(URL + LOGIN, loginData);
+
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      const result = await response.json();
+      console.log(result);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
     <div className="login-div">
       <LoginRegisterNav />
       <LoginRegisterSideBar picture="./img/login.png" />
       <div className="register-area mx-[200px] pt-[185px]">
         <h1 className="text-[40px] font-bold pb-[60px]">Login</h1>
-        <form action="">
+        <form action="" onSubmit={submitTest}>
           <Input
             label="Email"
             name="register-email"
@@ -19,6 +50,7 @@ function Login() {
             width="w-[450px]"
             spacing="pb-[24px]"
             placeholder="asep.bc@gmail.com"
+            onChange={(e) => setEmail(e.target.value)}
           />
           <Input
             label="Password"
@@ -27,6 +59,7 @@ function Login() {
             width="w-[450px]"
             spacing="pb-[24px]"
             placeholder="*********"
+            onChange={(e) => setPassword(e.target.value)}
           />
           <Button
             text="Submit"
