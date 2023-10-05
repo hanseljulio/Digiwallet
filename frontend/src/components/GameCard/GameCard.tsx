b34bceaffc7c946e2e224dd20db2b25ba97ed1f9
hansel.tjahyadi <hansel.tjahyadi@shopee.com>
2023-10-05 14:09:03 +0700

    games layout complete - working on card functionality

---
+++ frontend/src/components/GameCard/GameCard.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./GameCard.css";
+
+function GameCard() {
+  return (
+    <div className="gamecard-div">
+      <div className="gamecard-area w-[150px] h-[150px]"></div>
+    </div>
+  );
+}
+
+export default GameCard;
--- frontend/src/pages/Games/Games.tsx
+++ frontend/src/pages/Games/Games.tsx
@@ -3,10 +3,13 @@ import UserNav from "../../components/UserNav/UserNav";
 import { useStoreLoginPersist } from "../../store/store";
 import { URL } from "../../constants/constants";
 import GameSummary from "../../components/GamesSummary/GamesSummary";
+import GameCard from "../../components/GameCard/GameCard";
+import "./Games.css";
 
 function Games() {
   const stateLoginPersist = useStoreLoginPersist();
   const [summaryData, setSummaryData] = useState<string[]>([]);
+  const [chance, setChance] = useState<number>(3);
   const getUserData = async () => {
     try {
       const response = await fetch(URL + "/details", {
@@ -38,6 +41,25 @@ function Games() {
         accountNum={summaryData[1]}
         money={parseInt(summaryData[2])}
       />
+      <div className="flex-col games-section text-center pt-[48px]">
+        <h1 className="text-[40px] font-bold pb-[24px]">Games</h1>
+        <p className="text-[18px] pb-[12px]">
+          Choose a random box below to get a reward!
+        </p>
+        <p className="text-[18px] pb-[12px]">Chance: {chance}</p>
+        <p className="text-[18px] underline pb-[50px]">Check Leaderboard</p>
+        <div className="cards-section">
+          <GameCard />
+          <GameCard />
+          <GameCard />
+          <GameCard />
+          <GameCard />
+          <GameCard />
+          <GameCard />
+          <GameCard />
+          <GameCard />
+        </div>
+      </div>
     </div>
   );
 }
